d9acc90fe9678951d13f6a5c8021e5f1071265d9
Botan-linux <zeke000p@gmail.com>
2026-02-24 13:43:42 +0000

    Add files via upload

---
+++ botan-v1/airootfs/usr/local/bin/botan-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Kullanıcıyı oluştur (Fish shell ile ve gerekli gruplarla)
+useradd -m -p "" -g users -G wheel,audio,video,storage,optical,power -s /usr/bin/fish liveuser
+
+# Şifreyi boş ayarla (No password)
+passwd -d liveuser
+
+# Ana dizin izinlerini ayarla
+chown -R liveuser:users /home/liveuser
