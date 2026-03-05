4d4cfd62ba93a3cad657a57c81403ab6045ef4e5
Botan-linux <zeke000p@gmail.com>
2026-03-05 15:38:02 +0300

    Align all boot paths with LTS kernel and harden ISO defaults

--- profiledef.sh
+++ profiledef.sh
@@ -16,6 +16,7 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/skel/Desktop/install-botan.desktop"]="0:0:755"
   ["/usr/local/bin/botan-first-boot.sh"]="0:0:755"
+  ["/usr/local/bin/botan-net-config.sh"]="0:0:755"
   ["/etc/systemd/system/botan-init.service"]="0:0:644"
   ["/etc/calamares/"]="0:0:755"
   ["/usr/share/calamares/branding/botan/"]="0:0:755"
